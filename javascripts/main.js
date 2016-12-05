2fcedbc6788709b0b0534d3fab961f720d3ffb73
James Cordeiro <james@cordeiro.co.vu>
2016-12-05 10:23:41 +0000

    added readmes

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,8 @@ $(document).ready(function() {
                                                                 links = document.createElement("section"),
                                                                 devider = document.createElement("hr")
                                                                 svg = document.createElement("p"),
-                                                                links = document.createElement("section");
+                                                                links = document.createElement("section"),
+                                                                readmeDOM = document.createElement("div");
 
                                                         links.id = "downloads";
                                                         links.className = "clearfix";
@@ -24,7 +25,7 @@ $(document).ready(function() {
                                                         title.appendChild(document.createTextNode(repo.name.replace(/^lab-/ig, "")));
                                                         description.appendChild(document.createTextNode(repo.description));
 
-                                                        svg.innerHTML = '<div class="svg"><svg version="1.1" width="32" height="32" viewBox="0 0 16 16" class="octicon octicon-eye" aria-hidden="true"><use xlink:href="#eye" /></svg><div>' + intSeperator(repo.watchers_count) + '</div></div><div style="width: 10%;min-width: 100px;flex: 0 0 auto;box-sizing:border-box;padding:1em;text-align:center;float: left"><svg version="1.1" width="20" height="32" viewBox="0 0 10 16" class="octicon octicon-repo-forked" aria-hidden="true"><use xlink:href="#repo-forked" /></svg><div>' + intSeperator(repo.forks_count) + '</div></div><div class="svg"><svg version="1.1" width="32" height="32" viewBox="0 0 16 16" class="octicon octicon-bug" aria-hidden="true"><use xlink:href="#bug" /></svg><div>' + intSeperator(repo.open_issues_count) + '</div></div><div class="svg"><svg version="1.1" width="28" height="32" viewBox="0 0 14 16" class="octicon octicon-star" aria-hidden="true"><use xlink:href="#star" /></svg><div>' + intSeperator(repo.stargazers_count) + '</div></div><div class="svg"><svg version="1.1" width="24" height="32" viewBox="0 0 12 16" class="octicon octicon-person" aria-hidden="true"><use xlink:href="#person" /></svg><div>' + intSeperator(contributors.length) + '</div></div><div class="svg"><a href="' + repo.html_url + '" class="buttons" target="_BLANK"><svg version="1.1" width="24" height="32" viewBox="0 0 12 16" class="octicon octicon-link-external" aria-hidden="true"><use xlink:href="#link-external" /></svg><div><strong>Demo</strong></div></a></div><div class="clearfix"></div>';
+                                                        svg.innerHTML = '<div class="svg"><svg version="1.1" width="32" height="32" viewBox="0 0 16 16" class="octicon octicon-eye" aria-hidden="true"><use xlink:href="#eye" /></svg><div>' + intSeperator(repo.watchers_count) + '</div></div><div style="width: 10%;min-width: 100px;flex: 0 0 auto;box-sizing:border-box;padding:1em;text-align:center;float: left"><svg version="1.1" width="20" height="32" viewBox="0 0 10 16" class="octicon octicon-repo-forked" aria-hidden="true"><use xlink:href="#repo-forked" /></svg><div>' + intSeperator(repo.forks_count) + '</div></div><div class="svg"><svg version="1.1" width="32" height="32" viewBox="0 0 16 16" class="octicon octicon-bug" aria-hidden="true"><use xlink:href="#bug" /></svg><div>' + intSeperator(repo.open_issues_count) + '</div></div><div class="svg"><svg version="1.1" width="28" height="32" viewBox="0 0 14 16" class="octicon octicon-star" aria-hidden="true"><use xlink:href="#star" /></svg><div>' + intSeperator(repo.stargazers_count) + '</div></div><div class="svg"><svg version="1.1" width="24" height="32" viewBox="0 0 12 16" class="octicon octicon-person" aria-hidden="true"><use xlink:href="#person" /></svg><div>' + intSeperator(contributors.length) + '</div></div><div class="svg"><a href="https://marketlayer.github.io/' + repo.name + '" class="buttons" target="_BLANK"><svg version="1.1" width="24" height="32" viewBox="0 0 12 16" class="octicon octicon-link-external" aria-hidden="true"><use xlink:href="#link-external" /></svg><div><strong>Demo</strong></div></a></div><div class="clearfix"></div>';
 
                                                         links.innerHTML = '<a href="'+ repo.html_url +'/zipball/master" id="download-zip" class="button"><span>Download .zip</span></a><a href="'+ repo.html_url +'/tarball/master" id="download-tar-gz" class="button"><span>Download .tar.gz</span></a><a href="'+ repo.html_url +'" id="view-on-github" class="button" target="_BLANK"><span>View on GitHub</span></a>';
 
@@ -33,10 +34,34 @@ $(document).ready(function() {
                                                         document.getElementById("main_content").appendChild(svg);
                                                         document.getElementById("main_content").appendChild(links);
 
-                                                        results.splice(0, 1);
+                                                        $.ajax({
+                                                                url: repo.url + "/readme"
+                                                        }).done(function(readme) {
 
-                                                        if (results.length > 1) document.getElementById("main_content").appendChild(devider);
-                                                        getRepos();
+                                                                if (readme.content) {
+                                                                        readmeDOM.innerHTML = '<h5 class="showhidereadme"><a class="hideText" href="#">Show/Hide Readme</a></h5><br /><div class="readmeText hide">' + marked(b64DecodeUnicode(readme.content)) + "</div>";
+                                                                        readmeDOM.getElementsByClassName("showhidereadme")[0].onclick = function(e) {
+                                                                                switch(e.target.className) {
+                                                                                        case "showText":
+                                                                                                e.target.className = "hideText";
+                                                                                                readmeDOM.getElementsByClassName("readmeText")[0].className = "readmeText hide";
+                                                                                                break;
+                                                                                        case "hideText":
+                                                                                        default:
+                                                                                                e.target.className = "showText";
+                                                                                                readmeDOM.getElementsByClassName("readmeText")[0].className = "readmeText show";
+                                                                                }
+                                                                                return false;
+                                                                        }
+                                                                }
+
+                                                                document.getElementById("main_content").appendChild(readmeDOM);
+
+                                                                results.splice(0, 1);
+
+                                                                if (results.length > 1) document.getElementById("main_content").appendChild(devider);
+                                                                getRepos();
+                                                        });
                                                 });
                                         } else {
                                                 results.splice(0, 1);
@@ -53,4 +78,10 @@ $(document).ready(function() {
 
 function intSeperator(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function b64DecodeUnicode(str) {
+        return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
 }
